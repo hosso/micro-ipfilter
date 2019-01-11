3d4a2fe9c695355dc1a6674e9084ae9839d67232
hosso <hosso@users.noreply.github.com>
2019-01-11 16:59:09 +0900

    draft

--- example/index.js
+++ example/index.js
@@ -1,1 +1,7 @@
-module.exports = () => 'Hello!'
+const ipfilter = require('micro-ipfilter')
+
+const IP_FILTER = ['::1', '?(::ffff:)127.0.0.1', '?(::ffff:)192.168.0.*']
+
+module.exports = ipfilter({ filter: IP_FILTER }, (req, res) => {
+  res.end('Hello!')
+})
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello!')
+const { send } = require('micro')
+const isMatch = require('is-match')
+
+function ipfilter(options, handler) {
+  const matcher = isMatch(options.filter)
+
+  return (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
+    if (!matcher(ip)) {
+      send(res, 403, '403 Forbidden')
+      return
+    }
+
+    return handler(req, res)
+  }
+}
+
+module.exports = ipfilter
